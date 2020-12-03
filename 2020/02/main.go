3d3bb6e739f2b218c911ebd373513dc27304dbde
Evan do Carmo <carmo.evan@gmail.com>
2020-12-02 20:17:29 -0500

    day 2 part 2

--- 2020/02/main.go
+++ 2020/02/main.go
@@ -24,13 +24,31 @@ func isValidInput(line string) bool {
 	return false
 }
 
+func isValidInputTwo(line string) bool {
+	groups := r.FindStringSubmatch(line)
+	i, _ := strconv.Atoi(groups[1])
+	j, _ := strconv.Atoi(groups[2])
+	target := groups[3]
+	input := groups[4]
+	a := string(input[j - 1])
+	valid := false
+	if string(a) == target {
+		valid = !valid
+	}
+	b := input[i - 1]
+	if string(b) == target {
+		valid = !valid
+	}
+	return valid
+}
+
 func main() {
 	f, _ := os.Open("./input.txt")
 	scanner := bufio.NewScanner(f)
 	n := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		if isValidInput(line) {
+		if isValidInputTwo(line) {
 			n++
 		}
 	}
