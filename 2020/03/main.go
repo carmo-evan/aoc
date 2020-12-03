da1a8b445f3b0789f5602e1fcb2b5d2e59df3e12
Evan do Carmo <carmo.evan@gmail.com>
2020-12-03 12:04:25 -0500

    day 3 part 2

--- 2020/03/main.go
+++ 2020/03/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 )
+var right, down int
+func init() {
+	flag.IntVar(&right, "r", 1, "Right Increment Value")
+	flag.IntVar(&down, "d", 2, "Down Increment Value")
+}
 
 func growLine(line string, i int) string {
 	length := len(line)
@@ -16,19 +22,26 @@ func growLine(line string, i int) string {
 }
 
 func main() {
+	flag.Parse()
 	f, _ := os.Open("./input.txt")
 	scanner := bufio.NewScanner(f)
 	i := 0
 	l := 0
 	n := 0
 	for scanner.Scan() {
-		i += 3
-		l++
-		line := scanner.Text()
+		var line string
+		line = scanner.Text()
 		line = growLine(line, i)
 		if line[i] == '#' {
 			fmt.Printf("line %v, position %v: %v\n", l, i, line)
 			n++
+		}
+		i += right
+		l += down
+		if down > 1 {
+			for j := 0; j < (down - 1); j++ {
+				scanner.Scan()
+			}
 		}
 	}
 	fmt.Println(n)
